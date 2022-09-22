f0e824d42d83fc30c3abbb23102534ef3c2f8d3d
goktug5000 <69222008+goktug5000@users.noreply.github.com>
2022-09-23 02:08:35 +0300

    Add files via upload

---
+++ CharMove.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharMove : MonoBehaviour
+{
+    public float speed,MaxHP,HP,JumpPow;
+    public int MaxJump,jumpLeft;
+    public Text hpMiz;
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 5;
+        MaxHP = 10.5f;
+        HP = 10;
+        JumpPow = 10;
+        MaxJump=jumpLeft = 2;
+
+        hpMiz.text = (HP-HP%1).ToString() + " /" + (MaxHP-MaxHP%1).ToString();
+    }
+    public void ReCreate(float Fspeed,float FMaxHP,float FHP,float FJumpPow,int FMaxJump)
+    {
+        speed = Fspeed;
+        MaxHP = FHP;
+        HP = FHP;
+        JumpPow = FJumpPow;
+        MaxJump = FMaxJump;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+//HARAKET
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-speed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(speed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.W) && jumpLeft != 0)
+        {
+            this.gameObject.GetComponent<Rigidbody2D>().AddForce(transform.up * JumpPow * 20);
+            jumpLeft--;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0, -JumpPow * 2 * Time.deltaTime, 0);
+        }
+
+//YERE İNME
+
+    }
+
+
+    void FixedUpdate()
+    {
+        
+        // Cast a ray straight down.
+        
+        Vector3 v3 = new Vector3(this.gameObject.transform.position.x, this.gameObject.transform.position.y - 1.55f, this.gameObject.transform.position.z);
+        RaycastHit2D hit = Physics2D.Raycast(v3, -Vector2.up * 0.01f);
+        Debug.DrawRay(v3, -Vector2.up * 0.01f, Color.green);
+        // If it hits something...
+        if (hit.distance == 0)
+        {
+            jumpLeft = MaxJump;
+            //Debug.Log(hit.collider.gameObject.name);
+        }
+
+    }
+}
---
+++ ChooseOne.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChooseOne : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject Kolay;
+    public CharMove PlayerRN;
+    public float CSpeed, CHp, CmaxHp, CJumpPow;
+    public int CMaxJump;
+    public Text adim, genelAciklama, stats, skills;
+    void Start()
+    {
+        PlayerRN = GameObject.Find("Player").GetComponent<CharMove>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Zenci()
+    {
+        CSpeed = 15;
+        CmaxHp = 10;
+        CHp = 10;
+        CJumpPow = 20;
+        CMaxJump = 2;
+
+        adim.text = "Zenci";
+        genelAciklama.text = "Fiziken güçlü geçen ay köleydi.\n1 yerine 2 hoplar.";
+        stats.text = "Hız= " + CSpeed.ToString() + "\n"
+                    +"Max Can= "+CmaxHp.ToString()+"\n"
+                    +"Zıplama Gücü= "+CJumpPow.ToString()+"\n"
+                    ;
+
+
+    }
+    public void Beyaz()
+    {
+        CSpeed = 10;
+        CmaxHp = 10;
+        CHp = 10;
+        CJumpPow = 10;
+        CMaxJump = 1;
+
+        adim.text = "Beyaz";
+        genelAciklama.text = "Eskiden bir çiftçiydi.\nDiğerlerine göre daha zeki.";
+        stats.text = "Hız= " + CSpeed.ToString() + "\n"
+                    + "Max Can= " + CmaxHp.ToString() + "\n"
+                    + "Zıplama Gücü= " + CJumpPow.ToString() + "\n"
+                    ;
+
+
+    }
+
+    public void Sec()
+    {
+        //PlayerRN.ReCreate(speed, maxHP, HP,JumpPow);
+        PlayerRN.ReCreate(CSpeed, CmaxHp, CHp, CJumpPow, CMaxJump);
+    }
+}
